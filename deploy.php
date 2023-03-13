d2efe6092710d73e514c18d6bac4f06edc395e87
Moongazer <1685510+Moongazer@users.noreply.github.com>
2023-03-13 19:35:21 +0100

    Modify deploy config

--- deploy.php
+++ deploy.php
@@ -6,7 +6,7 @@ require 'recipe/symfony.php';
 
 // Config
 
-set('repository', 'git@github.com:Moongazer/lf-deploy-test.git');
+set('repository', 'https://github.com/Moongazer/lf-deploy-test.git'); // git@github.com:Moongazer/lf-deploy-test.git
 
 add('shared_files', []);
 add('shared_dirs', []);
@@ -17,7 +17,9 @@ add('writable_dirs', []);
 host('zircon')
     ->setHostname('w0192e57.kasserver.com')
     ->setRemoteUser('ssh-w01dd476')
-    ->setDeployPath('~/deploy-test');
+//    ->set('http_user', 'w01dd476') // @todo: why?
+    ->set('deploy_path', '/www/htdocs/w01dd476')
+    ->set('writable_mode', 'chmod'); // for shared-host environments where ACL isn't available/installable
 
 // Hooks
 
