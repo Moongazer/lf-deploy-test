56c4c788029799e0d77ee530cfbbe2db8b618537
Moongazer <1685510+Moongazer@users.noreply.github.com>
2023-03-13 17:30:09 +0100

    Edit deploy config

--- deploy.php
+++ deploy.php
@@ -14,8 +14,9 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('my-host-url.net')
-    ->set('remote_user', 'my-remoteuser')
+host('zircon')
+    ->hostname('w0192e57.kasserver.com')
+    ->set('remote_user', 'ssh-w01dd476')
     ->set('deploy_path', '~/deploy-test');
 
 // Hooks
