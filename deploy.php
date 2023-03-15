e10b630b177014ab34cb8d9966a57c51bf9a1f7d
H. Zirpel <h.zirpel@leuchtfeuer.com>
2023-03-15 16:36:23 +0100

    Add deploy config for testphp74

--- deploy.php
+++ deploy.php
@@ -14,12 +14,21 @@ add('writable_dirs', []);
 
 // Hosts
 
+host('testphp74')
+    ->setHostname('testphp74.office.leuchtfeuer.com')
+    ->setRemoteUser('zirpel')
+    ->set('branch', 'deploy/mautic')
+    ->set('deploy_path', '/var/www/zirpel/mautic-deploy-test.zirpel.testphp74')
+    ->set('writable_mode', 'chmod') // for shared-host environments where ACL isn't available/installable
+    ->set('composer_options', '--ignore-platform-reqs'); // @todo: resolve by installing PHP extensions (ext-bcmath, ext-imap)
+
 host('zircon')
     ->setHostname('w0192e57.kasserver.com')
     ->setRemoteUser('ssh-w01dd476')
     ->set('branch', 'deploy/mautic')
     ->set('deploy_path', '/www/htdocs/w01dd476')
-    ->set('writable_mode', 'chmod'); // for shared-host environments where ACL isn't available/installable
+    ->set('writable_mode', 'chmod') // for shared-host environments where ACL isn't available/installable
+    ->set('composer_options', '--ignore-platform-reqs'); // @todo: resolve by installing PHP extensions (ext-bcmath, ext-imap)
 
 // Hooks
 
